a27938cc95edf5cf299f7573ed89dc3792c5ab9b
Aksel Bell <aksel.bell@duke.edu>
2024-04-15 18:18:13 -0400

    wrote sieve of eratosthenes

--- sieve.py
+++ sieve.py
@@ -1,7 +1,25 @@
-def gcd(a,b): # Euclid's algorithm
-    if b == 0:
-        return a
-    elif a >= b:
-        return gcd(b,a % b)
-    else:
-        return gcd(b,a)
+def generatePrimes(B):
+    #sieve of Eratosthenes
+    if(B < 2):
+        return []
+
+    primes = [True] * (B + 1)
+    primes[0] = primes[1] = False
+    p = 2
+    
+    while(p*p <= B):
+        i = p*p
+        while(i <= B):
+            primes[i] = False
+            i = i + p
+        
+        p = p + 1
+        while not primes[p]:
+            p = p + 1
+    
+    print(primes)
+
+print("Enter a number to generate primes:")
+n = int(input())
+
+generatePrimes(n)
