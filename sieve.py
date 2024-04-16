9d6cc986087b917d2d3d019fe7d9cbe9ee628850
Aksel Bell <aksel.bell@duke.edu>
2024-04-15 20:29:10 -0400

    implemented check smooth function

--- sieve.py
+++ sieve.py
@@ -1,3 +1,25 @@
+import math
+
+def checkSmooth(n, primes):
+    # trial division pretty much, might be useful to add check at top if the number is B smooth using the stack forum
+    i = 0
+    exponents = [0] * len(primes)
+    while n != 0 and i < len(primes):
+        while n % primes[i] == 0:
+            exponents[i] += 1
+            n = n / primes[i]
+        i += 1
+    return exponents
+
+
+def gcd(x,y): # Euclid's algorithm
+     if y == 0:
+         return x
+     elif x >= y:
+         return gcd(y,x % y)
+     else:
+         return gcd(y,x) 
+
 def generatePrimes(B):
     #sieve of Eratosthenes
     if(B < 2):
@@ -17,9 +39,15 @@ def generatePrimes(B):
         while not primes[p]:
             p = p + 1
     
-    print(primes)
+    ret = []
+    for i in range(len(primes)):
+        if(primes[i]):
+            ret.append(i)
+
+    return ret
 
 print("Enter a number to generate primes:")
 n = int(input())
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
-generatePrimes(n)
+print(checkSmooth(n, primes))
