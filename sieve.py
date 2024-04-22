3fb1e79ce812036959129b5c0c2e6f43110d6df0
Aksel Bell <aksel.bell@duke.edu>
2024-04-22 14:40:01 -0400

    summed up exponent vectors and now works a bit better, still slightly bugged

--- sieve.py
+++ sieve.py
@@ -4,19 +4,19 @@ from datetime import datetime
 import math
 #import sympy
 
-#use 268905821 as an example input, it factors to 14347 * 18743
-#or 4994399
+#use 268905821 as an example input, it factors to 14347 * 18743. Doesn't work atm!
+#or 4994399 or 1491858941
 print("Enter a number to factor:")
 n = int(input())
 
 timeLastChecked = datetime.now()
 
-B = int(math.sqrt(n))
-#B = math.ceil(math.exp(0.5 * math.sqrt(math.log(n) * math.log(math.log(n)))))
+#B = int(math.sqrt(n))
+B = math.ceil(math.exp(0.5 * math.sqrt(math.log(n) * math.log(math.log(n)))))
 primes = generatePrimes(B)
 
-print(datetime.now() - timeLastChecked)
-timeLastChecked = datetime.now()
+# print(datetime.now() - timeLastChecked)
+# timeLastChecked = datetime.now()
 
 factorizations = dict() # map of each x term who's x^2 term is B smooth
 i = math.ceil(math.sqrt(n))
@@ -34,14 +34,14 @@ while len(factorizations) <= len(primes):
     
     i = i + 1
 
-print(datetime.now() - timeLastChecked)
-timeLastChecked = datetime.now()
+# print(datetime.now() - timeLastChecked)
+# timeLastChecked = datetime.now()
 
 transposedMatrix = transpose(matrix)
 nullSpace = null_space_mod2(transposedMatrix)
 
-print(datetime.now() - timeLastChecked)
-timeLastChecked = datetime.now()
+# print(datetime.now() - timeLastChecked)
+# timeLastChecked = datetime.now()
 
 linearCombination = nullSpace[0] # the linear combination of exponent vectors that sum to zero vector
 squares = []
@@ -50,8 +50,15 @@ for i in range(len(linearCombination)):
     if linearCombination[i] == 1:
         squares.append(keys[i])
 
-#print(squares)
+totalExponents = []
+for i in range(len(primes)):
+    entry = 0
+    for j in squares:
+        entry = entry + factorizations[j][i]
+    totalExponents.append(entry)
 
+print(totalExponents)
+    
 squaresProduct = 1
 primesProduct = 1
 
@@ -60,16 +67,19 @@ for i in range(len(squares)):
     
     exponents = factorizations[squares[i]]
     print(str(squares[i]) + "    " + str(exponents))
-    for j in range(len(exponents)):
-        if exponents[j] != 0:
-            primesProduct = primesProduct * (primes[j] ** exponents[j])    
 
-print((primesProduct - squaresProduct*squaresProduct)%n) # should ALWAYS be equal to zero
+for i in range(len(totalExponents)):
+    if totalExponents[i] != 0:
+        power = totalExponents[i] / 2  # Calculate the exponent
+        result = primes[i] ** power % n  # Compute the result modulo n
+        primesProduct = primesProduct * result
 
-primesProduct = math.sqrt(primesProduct)
-dif = abs(squaresProduct - primesProduct) % n
+print((squaresProduct*squaresProduct - primesProduct*primesProduct)%n) # should ALWAYS be equal to zero
+
+dif = abs(squaresProduct + primesProduct) % n
 
 print(dif)
+
 factor1 = gcd(dif, n)
 print(factor1)
 factor2 = n / factor1
